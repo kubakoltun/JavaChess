b0a670fb29739beea99fd22ba372e4b80f3c6db1
kubakoltun <87318086+kubakoltun@users.noreply.github.com>
2021-07-12 15:51:00 +0200

    Add files via upload

---
+++ src/com/chess/Chess.java
@@ -0,0 +1,16 @@
+package com.chess;
+
+import com.chess.engine.board.Board;
+import com.chess.gui.Table;
+
+public class Chess {
+
+    public static void main(final String[] args) {
+
+        Board board = Board.createStandardBoard();
+
+        System.out.println(board);
+
+        Table.get().show();
+    }
+}

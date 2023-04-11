73465a7513b1e4e7bb1ac98a69971f7a75f34fca
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-04-11 16:21:21 +0200

    Update Chess.java

--- src/com/chess/Chess.java
+++ src/com/chess/Chess.java
@@ -4,9 +4,7 @@ import com.chess.engine.board.Board;
 import com.chess.gui.Table;
 
 public class Chess {
-
     public static void main(final String[] args) {
-
         Table.get().show();
     }
 }
